9691a81a1d894f32201348ad425debd36333a00e
kronos <dua_bhavya@outlook.com>
2023-10-05 19:34:02 -0400

    Add routes for teams

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,23 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/teams' do
+    teams = Team.all
+    teams.to_json
+  end
+
+  post '/teams' do
+    team = Team.create(team_name: params[:team_name],
+      team_logo: params[team_logo],
+      wins: params[wins],
+      losses: params[losses]
+    )
+    team.to_json
+  end
+
+  patch '/teams/:id' do
+    team = Team.find(params[:id]).update(wins: params[:wins], losses: params[:losses])
+    team.to_json
   end
 
 end
