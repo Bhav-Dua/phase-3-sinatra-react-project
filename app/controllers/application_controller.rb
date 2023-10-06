425e328bb24e37b1278273907d83979b9d15ff6f
kronos <dua_bhavya@outlook.com>
2023-10-06 00:06:08 -0400

    Add routes for player

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,48 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/teams/:id' do
-    team = Team.find(params[:id]).update(wins: params[:wins], losses: params[:losses])
+    team = Team.find(params[:id])
+    team.update(wins: params[:wins], losses: params[:losses])
     team.to_json
   end
 
+  get '/players' do
+    players = Player.all
+    players.to_json
+  end
+
+  get '/players/:id' do
+    player = Player.find(params[:id])
+    player.to_json
+  end
+
+  get '/players/by_team/:id' do
+    players = Team.find(params[:id]).players
+    players.to_json
+  end
+
+  post '/players' do
+    player = Player.create(first_name: params[:first_name], 
+      last_name: params[:last_name],
+      age: params[:age],
+      team_id: params[:team_id]
+    )
+    player.to_json
+  end
+
+  patch '/players/:id' do
+    player = Player.find(params[:id])
+    player.update(first_name: params[:first_name], 
+    last_name: params[:last_name],
+    age: params[:age],
+    team_id: params[:team_id]
+    )
+    player.to_json
+  end
+
+  delete '/players/:id' do
+    player = Player.find(params[:id])
+    player.destroy
+    player.to_json
+  end
 end
